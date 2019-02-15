63f7178c43ca423f1b2da7f594b2202a09cec7c0
pfittante1001 <pfittante@yahoo.com>
2019-02-15 15:25:26 -0500

    started secuity

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -8,7 +8,7 @@ namespace SGearzAPI.API.Data
 
         public DataContext(DbContextOptions<DataContext> options) : base (options) {}
         
-         public DbSet<Customer> Customers { get; set; }   
+         public DbSet<Customer> Customers { get; set; }
          public DbSet<Product> Products { get; set; }
 
          public DbSet<Order> Orders { get; set; }
@@ -17,6 +17,8 @@ namespace SGearzAPI.API.Data
          public DbSet<Supplier> Suppliers { get; set; }
          public DbSet<Category> Categories { get; set; }
          public DbSet<ProductImage> ProductImages { get; set; }
+        public DbSet<User> Users { get; set; }
+
         
     }
 }
